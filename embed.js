1b3cc9f32c963640cc68213008a5a22ce3fe31fd
Di M <meerkdir@gmail.com>
2023-01-12 11:29:12 +0200

    changing value

--- embed.js
+++ embed.js
@@ -2,10 +2,10 @@
 if(location.pathname.includes('inflation-observer')) {
 
     let country_name = 'South Africa';
-    let last_full_year = '2019';
-    let latest_month = 'January 2020';
-    let annual = 4.5;
-    let monthly_change_yoy = 0.1;
+    let last_full_year = '2021';
+    let latest_month = 'January 2022';
+    let annual = 'something';
+    let monthly_change_yoy = 'something else';
 
     let country_text = `
     ${country_name}'s consumer price inflation (CPI) rate for the full year ${last_full_year} was ${annual}%. In ${latest_month} the annualised CPI was ${monthly_change_yoy}%. You can explore more details and the breakdown by category of goods in our ${country_name} Inflation Observer below.
