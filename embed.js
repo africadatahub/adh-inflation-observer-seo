4d3ba8481a9ff665b4f25de7d671d87400580165
Di M <meerkdir@gmail.com>
2023-01-12 13:46:25 +0200

    extra notes

--- embed.js
+++ embed.js
@@ -2102,14 +2102,14 @@ if(location.pathname.includes('inflation-observer')) {
             let country_name = country.location;
             let last_full_year = country_inflation_data.find(countryData => countryData.country_code == country.iso_code).last_full_year;
             let annual = country_inflation_data.find(countryData => countryData.country_code == country.iso_code).annual;
+            let extra_notes = country_inflation_data.find(countryData => countryData.country_code == country.iso_code).Extra_notes;
+            if(extra_notes != '') {
+                extra_notes = `<p>${extra_notes}</p>`;
+            }
 
-            let country_text = `<strong>${country_name}</strong>'s consumer price inflation (CPI) rate for the full year <strong>${last_full_year}</strong> was <strong>${Math.round(annual * 100) / 100}%</strong>.`;
+            let country_text = `<p><strong>${country_name}</strong>'s consumer price inflation (CPI) rate for the full year <strong>${last_full_year}</strong> was <strong>${Math.round(annual * 100) / 100}%. You can explore more details and the breakdown by category of goods in our <strong>${country_name}</strong> Inflation Observer below.</strong>.</p> ${extra_notes} <p>Want to see how ${country_name} compares to its neighbours? Select a new country to watch in the menu below.</p>`;
             const introPar = document.createElement("div");
-            introPar.style = "width: 100%;max-width: 1440px;margin: 1em auto; padding: 0 4em;font-size: 16px;";
-
-
-
-
+            introPar.style = "width: 100%;max-width: 1440px;margin: 1em auto; padding: 0 4em;font-size: 16px; text-align: center;";
 
             introPar.innerHTML = country_text;
             const parentDiv = document.getElementById("adh-embed").parentNode;
