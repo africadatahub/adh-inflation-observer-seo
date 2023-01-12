2cd98f45b81c6db2fd760ed0b00adfee1dedb026
Di M <meerkdir@gmail.com>
2023-01-12 11:12:20 +0200

    Added first text

--- embed.js
+++ embed.js
@@ -1,11 +1,23 @@
+
 if(location.pathname.includes('inflation-observer')) {
+
+    let country_name = 'South Africa';
+    let last_full_year = '2019';
+    let annual = 4.5;
+    let monthly_change_yoy = 0.1;
+
+    let country_text = `
+    ${country_name}'s consumer price inflation (CPI) rate for the full year ${last_full_year} was ${annual}%. In ${latest_month} the annualised CPI was ${monthly_change_yoy}%. You can explore more details and the breakdown by category of goods in our ${country_name} Inflation Observer below.
+    `
 
     // Create the new node to insert
     const introPar = document.createElement("span");
-    introPar.innerHTML = 'Inflation Observer';
+    introPar.innerHTML = country_text;
     const parentDiv = document.getElementById("adh-embed").parentNode;
     let embed = document.getElementById("adh-embed");
     parentDiv.insertBefore(introPar, embed);
+
+
     
 }
 
