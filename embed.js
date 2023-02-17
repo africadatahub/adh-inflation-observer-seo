d98d65ed3359dc927a433671a89211f81b86dad8
Di M <meerkdir@gmail.com>
2023-02-17 11:16:51 +0200

    Select work

--- embed.js
+++ embed.js
@@ -2088,6 +2088,8 @@ let country_inflation_data = [
 
 goToCountry = (e) => {
 
+  console.log(e);
+
     let country = e.target.value;
 
     if(country != '') {
@@ -2101,7 +2103,7 @@ if(location.pathname.includes('inflation-observer')) {
 
     let location_country = location.search.split('country=')[1];
 
-    let country_select = `<select onChange="goToCountry()"><option value="">Select your country</option>`;
+    let country_select = `<select onChange="goToCountry(e)"><option value="">Select your country</option>`;
     countries.forEach(country => {
         country_select += `<option value="${country.location}">${country.location}</option>`;
     });
